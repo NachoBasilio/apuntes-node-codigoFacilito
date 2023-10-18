2bc371afbd299a04fa9510c905cac05d8c0e8875
IgnaDev <arcortoon@gmail.com>
2023-10-17 21:38:18 -0300

    importar, exportar y dependencia

---
+++ ManosALaObra/index.js
@@ -0,0 +1,7 @@
+const {add, substract, combineArrayWithSymnol} = require('./modules/math.js');
+
+console.log("Hola")
+console.log(add(1, 2));
+console.log(substract(1, 2));
+console.log(combineArrayWithSymnol([1, 2, 3, 4, 5]));
+console.log(combineArrayWithSymnol([1, 2, 3, 4, 5], "x"));
---
+++ ManosALaObra/modules/math.js
@@ -0,0 +1,11 @@
+const lodash = require('lodash');
+
+const add = (a, b) => a + b;
+
+const substract = (a, b) => a - b;
+
+const combineArrayWithSymnol = (array, symbol = "-") => {
+    return lodash.join(array, symbol);
+}
+
+module.exports = {add, substract, combineArrayWithSymnol}
