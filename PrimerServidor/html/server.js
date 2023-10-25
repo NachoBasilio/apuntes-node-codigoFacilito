ba101837b66e283034c5bccd06ac40d11b3ba0c9
IgnaDev <arcortoon@gmail.com>
2023-10-25 18:39:50 -0300

    Agregando archivos estaticos

--- PrimerServidor/html/server.js
+++ PrimerServidor/html/server.js
@@ -2,6 +2,10 @@ const express = require('express');
 
 const app = express();
 
+
+
+app.use("/assets",express.static("assets"))
+
 app.get("/", function(req, res) {
     res.sendFile('index.html', { root: __dirname })
 })
