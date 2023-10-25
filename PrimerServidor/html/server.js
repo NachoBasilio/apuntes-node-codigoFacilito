f312c93b70c52c2beb44b2f4032984f24af22767
IgnaDev <arcortoon@gmail.com>
2023-10-25 19:06:23 -0300

    Cache

--- PrimerServidor/html/server.js
+++ PrimerServidor/html/server.js
@@ -4,7 +4,10 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use("/assets",express.static("assets"))
+app.use("/assets",express.static("assets",{
+    etag: false,
+    maxAge: "5h"//Esto es para que no se guarde en cache y se actualice cada 5 horas
+}))
 
 app.get("/", function(req, res) {
     res.render("index")
