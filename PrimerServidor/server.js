4f740b6a5af5ee3ec3d15cbd152ae9734f010a0f
IgnaDev <arcortoon@gmail.com>
2023-10-25 18:22:13 -0300

    post

--- PrimerServidor/server.js
+++ PrimerServidor/server.js
@@ -1,11 +1,16 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express()
+
+app.use(bodyParser.urlencoded({ extended: true }))//para que pueda leer los datos que vienen en le body de la petición
 
 app.get("/saludo", function(req, res) {
     res.send(`Hola ${req.query.name}!`)
 })
 
-
+app.post("/",function(req, res) {
+    res.send(`Hola ${req.body.name}!`)
+})
 
 app.listen(3000)
