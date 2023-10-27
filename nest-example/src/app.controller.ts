c9746463b56564e6b5d902d9088f9aadc1b71abf
IgnaDev <arcortoon@gmail.com>
2023-10-26 21:33:47 -0300

    Usando query params

--- nest-example/src/app.controller.ts
+++ nest-example/src/app.controller.ts
@@ -1,12 +1,24 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get('/test')
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('/api/items')
+  getItems(@Query() queries): any {
+    const esPar = queries.esPar;
+    const items = [1,2,3,4,5,6,7,8,9,10]
+
+    if(esPar === 'true'){
+      return items.filter(item => item % 2 === 0);
+    }else{
+      return items.filter(item => item % 2 !== 0);
+    }
   }
 }
