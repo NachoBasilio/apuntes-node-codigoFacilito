247f606fa6e6ddc60c3a6ac9b8ca89e8400ad963
IgnaDev <arcortoon@gmail.com>
2023-10-28 20:34:24 -0300

    Todo configurado para que comenzar con el CRUD

--- base_de_datos/server.js
+++ base_de_datos/server.js
@@ -1,18 +1,21 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const sqlite3 = require('sqlite3')
-
+const Sequelize = require('sequelize')
 
 const app = express()
 
 app.use(bodyParser.urlencoded({ extended: true }))//Para que pueda leer los datos que vienen del formulario
 
-let db = new sqlite3.Database("proyecto-backend")
+const sequelize = new Sequelize("proyecto-backend", null, null, {
+    dialect: "sqlite",
+    storage: "./proyecto-backend"
+}) 
 
 //db.run("CREATE TABLE tasks(id int AUTO_INCREMENT, description varchar(255))"); Una vez creada la tabla no es necesario volver a crearla
 
 app.post("/pendientes", function(req, res) {
-    db.run(`INSERT INTO tasks(description) VALUES(?)`, req.body.description)
+   // db.run(`INSERT INTO tasks(description) VALUES(?)`, req.body.description)
     res.send("Tarea creada")
 })
 
@@ -21,9 +24,3 @@ app.post("/pendientes", function(req, res) {
 
 app.listen(3000)
 
-//Con esto podemos cerrar el servidor con ctrl + c
-process.on("SIGINT", function(){
-    console.log("Cerrando el servidor")
-    db.close()
-    process.exit()
-})
