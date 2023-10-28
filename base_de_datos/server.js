db5a05123178a899460fff825114b37c3618ef6c
IgnaDev <arcortoon@gmail.com>
2023-10-28 19:51:42 -0300

    Agregado un valor a la base de datos

--- base_de_datos/server.js
+++ base_de_datos/server.js
@@ -1,7 +1,28 @@
+const express = require('express')
+const bodyParser = require('body-parser')
 const sqlite3 = require('sqlite3')
+
+const app = express()
+
+app.use(bodyParser.urlencoded({ extended: true }))//Para que pueda leer los datos que vienen del formulario
 
 let db = new sqlite3.Database("proyecto-backend")
 
-db.run("CREATE TABLE tasks(id int AUTO_INCREMENT, description varchar(255))");
+//db.run("CREATE TABLE tasks(id int AUTO_INCREMENT, description varchar(255))"); Una vez creada la tabla no es necesario volver a crearla
 
-db.close()
+app.post("/pendientes", function(req, res) {
+    db.run("INSERT INTO tasks(description) VALUES('Hello World')")
+    res.send("Tarea creada")
+})
+
+
+
+
+app.listen(3000)
+
+//Con esto podemos cerrar el servidor con ctrl + c
+process.on("SIGINT", function(){
+    console.log("Cerrando el servidor")
+    db.close()
+    process.exit()
+})
