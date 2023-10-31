ed41019be98d027aeed6a10db3fe7a7227dc3736
IgnaDev <arcortoon@gmail.com>
2023-10-30 23:32:12 -0300

    Primer modelo creado

--- base_de_datos/models/task.js
+++ base_de_datos/models/task.js
@@ -1,23 +1,9 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class Task extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-  }
-  Task.init({
+
+  let Task = sequelize.define('Task', {
     description: DataTypes.TEXT
-  }, {
-    sequelize,
-    modelName: 'Task',
-  });
+  }, {});
+
   return Task;
-};
+}
