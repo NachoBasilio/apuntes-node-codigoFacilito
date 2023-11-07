603482bc9f95721e0bee7b80f9e5998011dd6fe5
IgnaDev <arcortoon@gmail.com>
2023-11-07 19:52:24 -0300

    Obteniendo parametros

--- NestJSYoutube/xapi!/src/posts/posts.controller.ts
+++ NestJSYoutube/xapi!/src/posts/posts.controller.ts
@@ -1,9 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('posts')
 export class PostsController {
   @Get()
   getPosts() {
     return 'Hola gente';
+  }
+
+  @Get(':id')
+  getPost(@Param() params) {
+    return `Tu post id es ${params.id}`;
   }
 }
