109f374ce244425f2f910041b82b20ad933daacb
IgnaDev <arcortoon@gmail.com>
2023-11-14 18:02:01 -0300

    Utilizando queries

--- rest-ful-api/src/app.controller.ts
+++ rest-ful-api/src/app.controller.ts
@@ -1,12 +1,25 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, NotFoundException, Query } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get('/test')
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('/api/items')
+  getItems(@Query() queries): any {
+    const items = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+
+    const itemIndex = queries.item;
+
+    if (items[itemIndex] !== undefined) {
+      return items[itemIndex];
+    } else {
+      throw new NotFoundException(`Item with index ${itemIndex} not found`);
+    }
   }
 }
