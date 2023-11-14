503c22c02e5853d00e9bf3c7412ec5f73e3b78a6
IgnaDev <arcortoon@gmail.com>
2023-11-14 18:31:57 -0300

    Parametros

--- rest-ful-api/src/app.controller.ts
+++ rest-ful-api/src/app.controller.ts
@@ -1,4 +1,10 @@
-import { Controller, Get, NotFoundException, Query } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  NotFoundException,
+  Param,
+  Query,
+} from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -10,16 +16,27 @@ export class AppController {
     return this.appService.getHello();
   }
 
-  @Get('/api/items')
-  getItems(@Query() queries): any {
+  @Get('/api/item')
+  getItems(@Query('item') item): any {
     const items = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
 
-    const itemIndex = queries.item;
+    const itemIndex = parseInt(item);
 
     if (items[itemIndex] !== undefined) {
       return items[itemIndex];
     } else {
       throw new NotFoundException(`Item with index ${itemIndex} not found`);
     }
+  }
+
+  @Get('/api/items/:qty')
+  getItemsParams(@Param() allParams): any {
+    const items = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+
+    const itemIndex = parseInt(allParams.qty);
+
+    const ArrayCortado = items.slice(0, itemIndex);
+
+    return ArrayCortado;
   }
 }
