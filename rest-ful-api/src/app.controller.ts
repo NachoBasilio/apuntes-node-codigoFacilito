410acee1962bda5721b1e72acf177165ba561706
IgnaDev <arcortoon@gmail.com>
2023-11-15 18:31:32 -0300

    Tarea

--- rest-ful-api/src/app.controller.ts
+++ rest-ful-api/src/app.controller.ts
@@ -11,32 +11,18 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get('/test')
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('api/users')
+  getUsers() {
+    return this.appService.getUsers();
   }
 
-  @Get('/api/item')
-  getItems(@Query('item') item): any {
-    const items = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
-
-    const itemIndex = parseInt(item);
-
-    if (items[itemIndex] !== undefined) {
-      return items[itemIndex];
-    } else {
-      throw new NotFoundException(`Item with index ${itemIndex} not found`);
-    }
+  @Get('api/users/qty')
+  getUsersQTY() {
+    return this.appService.getUsersQTY();
   }
 
-  @Get('/api/items/:qty')
-  getItemsParams(@Param() allParams): any {
-    const items = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
-
-    const itemIndex = parseInt(allParams.qty);
-
-    const ArrayCortado = items.slice(0, itemIndex);
-
-    return ArrayCortado;
+  @Get('api/users/:id')
+  getUserById(@Param('id') id) {
+    return this.appService.getUsersById(id);
   }
 }
--- rest-ful-api/src/app.service.ts
+++ rest-ful-api/src/app.service.ts
@@ -2,7 +2,64 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  usuarios = [
+    {
+      id: 1,
+      nombre: 'Juan',
+      apellido: 'Gomez',
+      direccion: 'Calle 123, Ciudad XYZ',
+      estado_civil: 'Soltero',
+      pokemon_favorito: 'Pikachu',
+    },
+    {
+      id: 2,
+      nombre: 'Maria',
+      apellido: 'Rodriguez',
+      direccion: 'Avenida ABC, Ciudad ABC',
+      estado_civil: 'Casado',
+      pokemon_favorito: 'Charizard',
+    },
+    {
+      id: 3,
+      nombre: 'Carlos',
+      apellido: 'Lopez',
+      direccion: 'Carrera 456, Ciudad LMN',
+      estado_civil: 'Soltero',
+      pokemon_favorito: 'Bulbasaur',
+    },
+    {
+      id: 4,
+      nombre: 'Ana',
+      apellido: 'Martinez',
+      direccion: 'Calle 789, Ciudad PQR',
+      estado_civil: 'Casado',
+      pokemon_favorito: 'Squirtle',
+    },
+    {
+      id: 5,
+      nombre: 'Pedro',
+      apellido: 'Sanchez',
+      direccion: 'Avenida XYZ, Ciudad LMN',
+      estado_civil: 'Divorciado',
+      pokemon_favorito: 'Eevee',
+    },
+  ];
+
+  getUsers() {
+    return this.usuarios;
+  }
+
+  getUsersQTY() {
+    return this.usuarios.length;
+  }
+
+  getUsersById(id) {
+    const usuario = this.usuarios.find((user) => user.id == id);
+
+    if (usuario) {
+      return usuario;
+    } else {
+      return 'Todo mal';
+    }
   }
 }
