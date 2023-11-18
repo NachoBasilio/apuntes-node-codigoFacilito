3ae11f0e013793b5be37a9121ac3a8a52c6c7d75
IgnaDev <arcortoon@gmail.com>
2023-11-17 22:50:52 -0300

    Preparado para configurar mongo

--- rest-ful-api/src/app.module.ts
+++ rest-ful-api/src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HttpModule } from '@nestjs/axios';
 
 @Module({
-  imports: [HttpModule],
+  imports: [
+    HttpModule,
+    MongooseModule.forRoot('mongodb://localhost:27017/projects'),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
