4d09baca9feca080235b4ba1b9e5eb9d2f75bd4e
IgnaDev <arcortoon@gmail.com>
2023-11-17 23:10:39 -0300

    Creando un nuevo recurso

--- rest-ful-api/src/app.module.ts
+++ rest-ful-api/src/app.module.ts
@@ -4,11 +4,14 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HttpModule } from '@nestjs/axios';
+import { ProjectsModule } from './projects/projects.module';
+import { ProjectsModule } from './projects/projects.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost:27017/projects'),
     HttpModule,
+    ProjectsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
