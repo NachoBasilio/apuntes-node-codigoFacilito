078a1a7d31db9f41e90dff7c6064219a7a7c803d
IgnaDev <arcortoon@gmail.com>
2023-11-18 13:12:47 -0300

    Controllers creados y funcionando

--- rest-ful-api/src/projects/projects.controller.ts
+++ rest-ful-api/src/projects/projects.controller.ts
@@ -1,7 +1,47 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Param,
+  Get,
+  Post,
+  Delete,
+  Body,
+  Put,
+} from '@nestjs/common';
+
 import { ProjectsService } from './projects.service';
+import { CreateProjectDto } from './dto/create-project';
+import { UdateProjectDto } from './dto/update-project';
+import { Project } from './interfaces/project';
 
 @Controller('projects')
 export class ProjectsController {
   constructor(private readonly projectsService: ProjectsService) {}
+
+  @Get()
+  findAll(): Promise<Project[]> {
+    return this.projectsService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string): Promise<Project> {
+    return this.projectsService.FindOne(id);
+  }
+
+  @Post()
+  create(@Body() createProductDto: CreateProjectDto): Promise<Project> {
+    return this.projectsService.create(createProductDto);
+  }
+
+  @Put(':id')
+  update(
+    @Param('id') id: string,
+    @Body() updateProductDto: UdateProjectDto,
+  ): Promise<Project> {
+    return this.projectsService.update(id, updateProductDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string): Promise<Project> {
+    return this.projectsService.remove(id);
+  }
 }
