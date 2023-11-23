fb272cda105d78bc5b87495f7654700cc2b5f98c
IgnaDev <arcortoon@gmail.com>
2023-11-22 21:08:52 -0300

    Terminado de configurar

---
+++ rest-ful-api/src/auth/auth.controller.ts
@@ -0,0 +1,13 @@
+import { Controller, Request, Post, UseGuards } from '@nestjs/common';
+import { LocalAuthGuard } from './local-auth.guard';
+
+@Controller('auth')
+export class AuthController {
+  constructor() {}
+
+  @UseGuards(LocalAuthGuard)
+  @Post('login')
+  login(@Request() req) {
+    return req.user;
+  }
+}
