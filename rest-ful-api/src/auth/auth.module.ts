e4104e07c3d094307e4bf10ade6f597d21243e95
IgnaDev <arcortoon@gmail.com>
2023-11-24 14:36:37 -0300

    Flujo completo para JWT

--- rest-ful-api/src/auth/auth.module.ts
+++ rest-ful-api/src/auth/auth.module.ts
@@ -2,9 +2,16 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LocalStrategy } from './strategies/local.strategy';
 import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [PassportModule],
+  imports: [
+    PassportModule,
+    JwtModule.register({
+      secret: 'POK4M0NM4ND40-pidg4y',
+      signOptions: { expiresIn: '20s' },
+    }),
+  ],
   providers: [AuthService, LocalStrategy],
   exports: [AuthService],
 })
