afae17ca22029894c86a712ae0501d42789fca1e
IgnaDev <arcortoon@gmail.com>
2023-12-12 11:50:18 -0300

    Tarea completada

--- cf-websocket-homework/src/app.module.ts
+++ cf-websocket-homework/src/app.module.ts
@@ -2,20 +2,18 @@ import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
 import { Chat2Module } from './chat2/chat2.module';
+import { ChatModule } from './chat/chat.module';
 
-
-
-
-@Module({  
-
+@Module({
   imports: [
-
     // Esto habilita la carpeta publica para que se pueda acceder desde el navegador
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
     }),
 
     Chat2Module,
-  ]
+
+    ChatModule,
+  ],
 })
 export class AppModule {}
